37224f42323d80d4ab095b331374dbe2e78af757
Jakub <jakub.piskorz@gazeta.pl>
2022-12-31 05:07:00 +0100

    fix?

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import {
+  Controller,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+// type AuthService = any;
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('login')
+  login(@Body() dto: AuthDto) {
+    console.log({ dto });
+    return this.authService.loginService(dto);
+  }
+
+  @Post('register')
+  register(@Body() dto: AuthDto) {
+    return this.authService.registerService(dto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+  ) {}
+
+  async loginService(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user =
+        await this.prismaService.user.create({
+          data: {
+            email: dto.email,
+            hash,
+          },
+        });
+
+      delete user.hash;
+
+      return user;
+    } catch (e) {
+      if (
+        e instanceof PrismaClientKnownRequestError
+      ) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+    }
+  }
+
+  async registerService(dto: AuthDto) {
+    const user =
+      await this.prismaService.user.findUnique({
+        where: { email: dto.email },
+      });
+    if (!user) {
+      throw new ForbiddenException(
+        'Credentials incorrect',
+      );
+    }
+
+    const passwordMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    if (!passwordMatches) {
+      throw new ForbiddenException(
+        'Password incorrect',
+      );
+    }
+
+    return user;
+  }
+}
