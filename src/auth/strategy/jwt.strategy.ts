ddaef41d073e457d1b6e58cefb1513bf2d5148ea
Jakub <jakub.piskorz@gazeta.pl>
2023-01-06 22:10:09 +0100

    feat: Bearer auth token + users/me

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -9,6 +9,7 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(
   Strategy,
+  'jwt',
 ) {
   constructor(config: ConfigService) {
     super({
@@ -16,5 +17,8 @@ export class JwtStrategy extends PassportStrategy(
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+  validate(payload: any) {
+    return payload;
   }
 }
